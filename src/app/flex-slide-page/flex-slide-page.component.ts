0edbe1afa80a90ac68cf7bf0664964744c410d92
Bhatti <Parminder.Bhatti@linkgroup.co.uk>
2018-03-22 14:11:25 +0000

    jquery removed from the flex-slide-page

--- src/app/flex-slide-page/flex-slide-page.component.ts
+++ src/app/flex-slide-page/flex-slide-page.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FlexSlidePageComponent implements OnInit {
 
+  public pageConstructs = [
+    { id: 1, open: false, heading: 'Welcome to the site', image: '/assets/images/logo.png' },
+    { id: 2, open: false, heading: 'Welcome to the site2', image: '/assets/images/logo.png' },
+    { id: 3, open: false, heading: 'Welcome to the site3', image: '/assets/images/logo.png' },
+    { id: 4, open: false, heading: 'Welcome to the site4', image: '/assets/images/logo.png' },
+    { id: 5, open: false, heading: 'Welcome to the site5', image: '/assets/images/logo.png' },
+  ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleActive(id) {
+    this.pageConstructs.forEach(page => {
+      if (page.id === id) {
+        page.open = true;
+      } else {
+        page.open = false;
+      }
+    });
+  }
 }
