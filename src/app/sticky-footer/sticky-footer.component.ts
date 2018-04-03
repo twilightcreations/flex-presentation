3f85577da067d7ca1d4502b5000f65d0a1ba7622
Bhatti <Parminder.Bhatti@linkgroup.co.uk>
2018-04-03 16:07:57 +0100

    sticky footer page updated

--- src/app/sticky-footer/sticky-footer.component.ts
+++ src/app/sticky-footer/sticky-footer.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class StickyFooterComponent implements OnInit {
 
   constructor() { }
-
+  moreText: boolean = true;
+ 
   ngOnInit() {
   }
 
