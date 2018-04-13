1e5ab393082df8b61f69d9756f824da903c57d39
P Bhatti <pbhatti@live.co.uk>
2018-04-13 21:51:23 +0100

    layout of the control

--- src/app/to-do-list/todo-item/todo-item.component.ts
+++ src/app/to-do-list/todo-item/todo-item.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
-  selector: 'app-todo-item',
+  selector: '[app-todo-item]',
   templateUrl: './todo-item.component.html',
   styleUrls: ['./todo-item.component.scss']
 })
