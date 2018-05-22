97ab615b28db1ac6b9253dc9b0fa85430b788740
P Bhatti <pbhatti@live.co.uk>
2018-05-22 22:42:22 +0100

    instantiated the variable in the .ts file

--- src/app/holding-page/holding-page.component.ts
+++ src/app/holding-page/holding-page.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HoldingPageComponent implements OnInit {
 
   jsfloat: boolean;
-
+  contacts: string;
+  
   constructor() { }
 
   ngOnInit() {
